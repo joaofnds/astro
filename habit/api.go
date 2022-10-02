746ec7914a9b754b7174cdeb696e053e25555425
João Fernandes <joaofnds@joaofnds.com>
2022-10-02 11:24:05 -0300

    refactor: api requests

--- habit/api.go
+++ habit/api.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"net/http"
 	"net/url"
-	"strings"
 )
 
 type Headers = map[string]string
@@ -21,76 +20,70 @@ func NewAPI() *API {
 }
 
 func (a API) List(token string) (*http.Response, error) {
-	return get(a.baseURL+"/habits", map[string]string{"Authorization": token})
+	return req(
+		http.MethodGet,
+		a.baseURL+"/habits",
+		map[string]string{"Authorization": token},
+		new(bytes.Buffer),
+	)
 }
 
 func (a API) Create(token, name string) (*http.Response, error) {
-	return post(
+	return req(
+		http.MethodPost,
 		a.baseURL+"/habits?name="+url.QueryEscape(name),
 		Headers{"Content-Type": "application/json", "Authorization": token},
-		&bytes.Buffer{},
+		new(bytes.Buffer),
 	)
 }
 
 func (a API) Get(token, id string) (*http.Response, error) {
-	return get(
+	return req(
+		http.MethodGet,
 		a.baseURL+"/habits/"+id,
 		map[string]string{"Authorization": token},
+		new(bytes.Buffer),
 	)
 }
 
 func (a API) Delete(token, id string) (*http.Response, error) {
-	return delete(
+	return req(
+		http.MethodDelete,
 		a.baseURL+"/habits/"+id,
 		map[string]string{"Authorization": token},
+		new(bytes.Buffer),
 	)
 }
 
 func (a API) AddActivity(token, id string) (*http.Response, error) {
-	return post(
+	return req(
+		http.MethodPost,
 		a.baseURL+"/habits/"+id,
 		map[string]string{"Authorization": token},
-		&bytes.Buffer{},
+		new(bytes.Buffer),
 	)
 }
 
 func (a API) CreateToken() (*http.Response, error) {
-	return http.Post(a.baseURL+"/token", "application/text", strings.NewReader(""))
+	return req(
+		http.MethodPost,
+		a.baseURL+"/token",
+		map[string]string{"Content-Type": "application/text"},
+		new(bytes.Buffer),
+	)
 }
 
-func (a API) TestToken(token []byte) (*http.Response, error) {
-	req, err := http.NewRequest(http.MethodGet, a.baseURL+"/tokentest", strings.NewReader(""))
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Authorization", string(token))
-	return http.DefaultClient.Do(req)
+func (a API) TestToken(token string) (*http.Response, error) {
+	return req(
+		http.MethodGet,
+		a.baseURL+"/tokentest",
+		map[string]string{"Authorization": token},
+		new(bytes.Buffer),
+	)
 }
 
-func get(url string, headers Headers) (*http.Response, error) {
-	req, err := http.NewRequest(http.MethodGet, url, new(bytes.Buffer))
-	if err != nil {
-		return nil, err
-	}
-	for k, v := range headers {
-		req.Header.Add(k, v)
-	}
-	return http.DefaultClient.Do(req)
-}
-
-func post(url string, headers Headers, body io.Reader) (*http.Response, error) {
-	req, err := http.NewRequest(http.MethodPost, url, &bytes.Buffer{})
-	if err != nil {
-		return nil, err
-	}
-	for k, v := range headers {
-		req.Header.Add(k, v)
-	}
-	return http.DefaultClient.Do(req)
-}
-
-func delete(url string, headers Headers) (*http.Response, error) {
-	req, err := http.NewRequest(http.MethodDelete, url, &bytes.Buffer{})
+func req(method string, url string, headers map[string]string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		return nil, err
 	}
